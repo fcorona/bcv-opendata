87280c675f80ee4024663f5f75f025615e2ad519
Agares <ekeisco@gmail.com>
2013-11-05 14:32:56 -0500

    #3.1.2.1 importa un archivo

--- app/server.js
+++ app/server.js
@@ -1,11 +1,31 @@
-var express = require('express');
+var express = require('express'),
+    path = require('path');
+    fs = require('fs');
+
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res){
-  var body = 'OpenData';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+  res.render('index', {title:'Plataforma de openData'});
+});
+
+app.get('/admin/upload/', function(req, res){
+  console.log(req);
+  res.render('upload', {title:'Plataforma de openData'});
+});
+
+app.post('/admin/upload/', function(req, res){
+  
+  fs.readFile(req.files.file.path, {encoding:'utf8'}, function (err, data) {
+    console.log(data);
+  });
+  res.redirect('/admin/upload/');
 });
 
 app.listen(3000);
