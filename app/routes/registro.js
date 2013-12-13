f14b1a235dbbf5c7d8971d2b7a839a09d651af85
Cristian Fabricio Rodriguez Romero <fab7696650@hotmail.com>
2013-12-13 12:28:28 -0500

    Se crea la validacion de existencia de usuario en el registro
    Se modifica la validacion de registro, buscando si el correo enviado ya esta registrado en la plataforma

--- app/routes/registro.js
+++ app/routes/registro.js
@@ -3,19 +3,26 @@ var mongoose = require('mongoose'),
     model_user = schema.User,
     bcrypt = require('bcrypt-nodejs');
 //se busca si existe el usuario
+function registrar(req,res){
+      if(req.body.pass==req.body.repass){
+          var user = new model_user({ 
+              name:req.body.name,
+              email:req.body.email,
+              password : bcrypt.hashSync(req.body.pass)
+          });
+          user.save();
+          res.render('register', {msg:'Usuario registrado'});
+      }else{
+          res.render('register', {notpass:true});
+      }
+    }
 //se guarda el registro del usuario
 exports.registro = function(req,res){
-    if(req.body.pass==req.body.repass){
-        var user = new model_user({ 
-            name:req.body.name,
-            email:req.body.email,
-            password : bcrypt.hashSync(req.body.pass)
+    model_user.findOne({ email : req.body.email }, function(err, userx){
+        if (err) {console.log(err)};
+        if (!userx){return registrar(req,res)};
+        res.render('register',{msg:'Este usuario ya esta registrado', register:true});
         });
-        user.save();
-        res.render('register', {msg:'Usuario registrado'});
-    }else{
-        res.render('register', {notpass:true});
-        }
 }; 
 //se hace la solicitud del formulario de registro
 exports.formulario = function(req,res){
