b4d8ea6e57e2ab2eb44040af39910e72911052a1
David Roncancio <david.roncancio@gmail.com>
2013-12-17 21:51:06 -0500

    Deploy script v1.0

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ bcv-opendata
 ============
 
 Proyecto de Open Data para Bogotá cómo vamos?
+
+
+### Deploy
+
+Enable SSH agent forwarding https://help.github.com/articles/using-ssh-agent-forwarding to <<put domain here>>
+
+This will only work if you have deploy permission and permission to clone the repository
+
+	./deploy production
--- app/server.js
+++ app/server.js
@@ -107,5 +107,5 @@ app.post('/login', passport.authenticate('local', { successRedirect: '/',
                                                     failureRedirect: '/login' }));
 app.get('/login', login.login);
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000);
+console.log('Listening on port ' + process.env.PORT || 3000);
