1b68787fd07c978497ec6e3ed047fac2c0baff76
Agares <ekeisco@gmail.com>
2013-12-18 23:55:57 -0500

    agrega validacion de registro y estilos

--- app/server.js
+++ app/server.js
@@ -1,18 +1,18 @@
 var env = require('dotenv'); env.load();
 var express = require('express'),
-    path = require('path'),
-    flash = require('connect-flash'),
-    admin = require('./routes/admin'),
-    admin2 = require('./routes/admin2'),
-    api = require('./routes/api'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    registro = require('./routes/registro'),
-    login = require('./routes/login'),
-    schema = require('./models/user'),
-    User_model = schema.User,
-    dataset = require('./routes/dataset'),
-    baucis = require('baucis');
+path = require('path'),
+flash = require('connect-flash'),
+admin = require('./routes/admin'),
+admin2 = require('./routes/admin2'),
+api = require('./routes/api'),
+passport = require('passport'),
+LocalStrategy = require('passport-local').Strategy,
+registro = require('./routes/registro'),
+login = require('./routes/login'),
+schema = require('./models/user'),
+User_model = schema.User,
+dataset = require('./routes/dataset'),
+baucis = require('baucis');
 
 var app = express();
 
@@ -27,38 +27,44 @@ app.use(flash());
 
 //use of passport
 app.configure(function() {
-    app.use(express.static('public'));
-    app.use(express.cookieParser());
-    app.use(express.bodyParser());
-    app.use(express.session({ secret: 'keyboard cat' }));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(app.router);
+  app.use(express.static('public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(function (req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    console.log(res.locals.login);
+    next();
+  });
+  app.use(app.router);
 });
 passport.use(new LocalStrategy(function(username, password, done) {
-    User_model.findOne({ email: username }, function(err, userx) {
-        if (err) { 
-            return done(err); 
-        }
-        if (!userx) {
-            return done(null, false, { message: 'Incorrect email.' });
-        }
-        if (!userx.validPassword(password)) {
-            return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, userx);
-      });
+  User_model.findOne({ email: username }, function(err, userx) {
+    if (err) { 
+      return done(err); 
     }
+    if (!userx) {
+      return done(null, false, { message: 'Incorrect email.' });
+    }
+    if (!userx.validPassword(password)) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, userx);
+  });
+}
 ));
 passport.serializeUser(function(user, done) {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-    User_model.findById(id, function(err, user) {
-        done(err, user);
-    });
+  User_model.findById(id, function(err, user) {
+    done(err, user);
+  });
 });
+
 //end
 /*
 * routing
@@ -67,7 +73,7 @@ app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
- });
+});
 
 app.get('/', function(req, res){
   res.render('index', {title:'Plataforma de openData'});
@@ -105,8 +111,13 @@ app.post('/registro', registro.registro);
 
 //login
 app.post('/login', passport.authenticate('local', { successRedirect: '/',
-                                                    failureRedirect: '/login' }));
+  failureRedirect: '/login' }));
 app.get('/login', login.login);
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 app.listen(process.env.PORT || 3000);
 console.log('Listening on port %s', process.env.PORT || 3000);
