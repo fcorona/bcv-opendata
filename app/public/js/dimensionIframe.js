95bfb4c72878bb7fd7904cd1c0e5f579369ef765
Cristian Fabricio Rodriguez Romero <fab7696650@hotmail.com>
2014-02-23 20:57:47 -0500

    prueba redimensionamiento canvas

--- app/public/js/dimensionIframe.js
+++ app/public/js/dimensionIframe.js
@@ -3,7 +3,7 @@ function dimension_iframe(){
 	var ancho_actual = window.innerWidth;
 	var alto_actual = window.innerHeight;
 	var iframe = d3.select("#content-graphid");
-	iframe.attr('height', 4000 /*alto_actual*/);
+	iframe.attr('height', 1000/* ancho_actual*0.75*/);
 	iframe.attr('width', ancho_actual*0.7);
 }
 window.onload=dimension_iframe;
