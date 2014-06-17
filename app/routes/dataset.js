368012a167a6c1b11e6e133736f782b876acfd2d
Gustavo Andrés Angulo <woakas@gmail.com>
2014-06-17 18:02:31 -0500

    Fixed CSV export for subjective data

--- app/routes/dataset.js
+++ app/routes/dataset.js
@@ -2,6 +2,7 @@ var flash = require('connect-flash'),
     dataset = require('../models/dataset'),
     MetricModel = require('../models/metric').MetricModel,
     METRIC_VIAS = require('../models/metric').METRIC_VIAS,
+    subjective = require('../models/subjectiveData'),
     Iconv = require('iconv').Iconv,
     csv = require('csv');
 
@@ -52,6 +53,74 @@ var exportCSV = function(foundDataset, res){
     });
   });
 }
+
+
+var exportCSVSubjective = function(foundDataset, res){
+  var headers = ['Dimensión', 'Categoría','Indicador','Descripción', 'Año'];
+  var content = [];
+  content.push(foundDataset.dimension ? foundDataset.dimension.name : '');
+  content.push(foundDataset.category ? foundDataset.category.name : '');
+  content.push(foundDataset.name);
+  content.push(foundDataset.description);
+  for (var i = 0; i < foundDataset.optionValues.length; i++) {
+    headers.push(foundDataset.optionValues[i].description);
+  }
+
+  var name= foundDataset.name.toLowerCase();
+  subjective.retrieveDataFrom(name, {},function (err, results){
+      if (err) console.log(err);
+      
+      var datasByYear = {};
+      for(var i=0; i<results.length; i++){
+        var result = results[i];
+        var valuesByYear = {};
+        for(var j=0; j<result.values.length; j++){
+          var values = result.values[j];
+          if(!values.option || values.option===''){
+            valuesByYear['empty'] = values.total + (valuesByYear['empty']||0);
+          }
+          if(values.option.indexOf('|')!=-1){
+            var multipleValues = values.option.split('|');
+            for(var k=0; k<multipleValues.length; k++){
+              valuesByYear[multipleValues[k]] = values.total + (valuesByYear[multipleValues[k]]||0);
+            }
+          }else{
+            valuesByYear[values.option] = values.total + (valuesByYear[values.option]||0);
+          }
+        }
+        datasByYear[''+result['_id']] = valuesByYear;
+      }
+      var rows = [headers];
+      
+      for (var i in datasByYear){
+          var r_data = [i];
+          for (var ov = 0; ov < foundDataset.optionValues.length; ov++) {
+              r_data.push(datasByYear[i][foundDataset.optionValues[ov].option]);
+          }
+          rows.push(content.concat(r_data));
+          
+      }
+      
+      res.set('Content-Type', 'text/csv');
+      res.set('Content-Disposition', 'attachment; filename='+foundDataset.name+'.csv');
+      var iconv = new Iconv('UTF-8', 'ISO-8859-1');
+      var result = [];
+      csv()
+      .from(rows, { delimiter: ';'})
+      .on('data', function(data) {
+          result.push(data);
+      })
+      .on('end', function() {
+          var buffer = iconv.convert(result.join(''));
+          res.send(buffer);
+      });
+      
+
+  });
+
+    
+
+};
 
 
 var exportAllCSV = function(res){
@@ -144,9 +213,15 @@ exports.showDataset = function(req, res, next){
     }
     MetricModel.saveMetric(METRIC_VIAS[format], null, foundData['_id']);
     if(format==='csv'){
-      exportCSV(foundData, res);
-      return;
+      if(foundData.dataset.type==1){        
+        exportCSV(foundData, res);
+        return;
+      }else{
+        exportCSVSubjective(foundData, res);
+          return;
+      }
     }
+        
     if(foundData.dataset.type==1){
       res.render('citizen/datasetHtml', {
         title: foundData.name,
